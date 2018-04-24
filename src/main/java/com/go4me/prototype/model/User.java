a5e7014cc20e92aead8121f6e48c1462392d08a5
Alkesst <alec.augal@gmail.com>
2018-04-24 14:18:06 +0200

    Added the attributes in the User Class

--- src/main/java/com/go4me/prototype/model/User.java
+++ src/main/java/com/go4me/prototype/model/User.java
@@ -1,14 +1,31 @@
 package com.go4me.prototype.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
+    private boolean isPremium = false;
+
+    @Column(unique=true, nullable = false)
+    private String userName;
+
+    @Column(unique=true, nullable=false)
+    private String email;
+
+    private List<User> blockedUsers;
+    private boolean isBanned;
+
+    @Column(unique=true)
+    private String twitterAccount;
+
+    /*TODO ADD PUBLISHED ORDERS AND ADS ORDER VARIABLES*/
+
+
 }
--- src/main/java/com/go4me/prototype/model/UserRepository.java
+++ src/main/java/com/go4me/prototype/model/UserRepository.java
@@ -1,9 +1,9 @@
 package com.go4me.prototype.model;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findById(String id);
 }
