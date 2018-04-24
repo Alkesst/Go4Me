6aed7fca00e35f7cf148ba3d1fb5ae5080154c8a
jesuspa98 <jesusparejo98@gmail.com>
2018-04-24 17:53:08 +0200

    Fixes document 5Modelado and added Localization's getters and setters

--- src/main/java/com/go4me/prototype/model/Localization.java
+++ src/main/java/com/go4me/prototype/model/Localization.java
@@ -26,4 +26,86 @@ public class Localization {
     private double latitude;
 
     private double longitude;
+
+    public Localization(){}
+
+    public Localization(String address1, String city, String country, int zipCode) {
+        this.address1 = address1;
+        this.city = city;
+        this.country = country;
+        this.zipCode = zipCode;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+
+    //TODO Este método debe realizarse con la colaboración de los integrantes del grupo en la proxima reunion
+    public void searchOrderLocation(double longitude, double latitude){
+
+    }
+
+
+
 }
