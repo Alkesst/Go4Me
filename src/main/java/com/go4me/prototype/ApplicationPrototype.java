32b2bdc29cfbbc86a55814b9b2140f4023d360ed
Alkesst <alec.augal@gmail.com>
2018-04-24 13:07:32 +0200

    Added basic structure to the project and a litle hello world

---
+++ src/main/java/com/go4me/prototype/ApplicationPrototype.java
@@ -0,0 +1,13 @@
+package com.go4me.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationPrototype {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationPrototype.class, args);
+    }
+
+
+}
