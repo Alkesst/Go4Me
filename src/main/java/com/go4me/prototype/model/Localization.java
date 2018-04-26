5bafdd649d9d4742c8c7336146c77addbbe66b8e
Alkesst <alec.augal@gmail.com>
2018-04-26 11:10:26 +0200

    Added parser form address to Lat Lng. Fixes #20

--- src/main/java/com/go4me/prototype/model/Localization.java
+++ src/main/java/com/go4me/prototype/model/Localization.java
@@ -1,10 +1,18 @@
 package com.go4me.prototype.model;
 
 
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
+
 import javax.persistence.*;
+import java.io.IOException;
 
 @Entity
 public class Localization {
+    private static final String API_KEY = "AIzaSyBGSymDT45SwboBEgG_D53iAFhDurwGlww";
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -30,10 +38,16 @@ public class Localization {
     public Localization(){}
 
     public Localization(String address1, String city, String country, int zipCode) {
+        LatLng coord;
         this.address1 = address1;
         this.city = city;
         this.country = country;
         this.zipCode = zipCode;
+        coord = getCoordinates(address1);
+        if(coord != null){
+            this.latitude = coord.lat;
+            this.longitude = coord.lng;
+        }
     }
 
     public Long getId(){
@@ -106,6 +120,18 @@ public class Localization {
 
     }
 
-
-
+    private static LatLng getCoordinates(String address){
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey(API_KEY)
+                .build();
+        GeocodingResult[] results;
+        try {
+            results = com.google.maps.GeocodingApi.geocode(context,
+                    address).await();
+            return results[0].geometry.location;
+        } catch (ApiException | InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
